7daf3354825ee475a546d4f9ded4f2c314213ba7
Inna Ladyzhenskaia <pinguinil2000@gmail.com>
2023-10-28 07:28:12 +0300

    добавляет карту и создает сетки для футера

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 let headerIndex = document.querySelector('.header__index');
+let headerInner = document.querySelector('.header__inner');
 let headerToggle = document.querySelector('.header__toggle');
 
 headerIndex.classList.remove('header__index--nojs');
@@ -10,5 +11,17 @@ headerToggle.addEventListener('click', function () {
   } else {
     headerIndex.classList.add('header__index--closed');
     headerIndex.classList.remove('header__index--opened');
+  }
+});
+
+headerInner.classList.remove('header__inner--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (headerInner.classList.contains('header__inner--closed')) {
+    headerInner.classList.remove('header__inner--closed');
+    headerInner.classList.add('header__inner--opened');
+  } else {
+    headerInner.classList.add('header__inner--closed');
+    headerInner.classList.remove('header__inner--opened');
   }
 });
