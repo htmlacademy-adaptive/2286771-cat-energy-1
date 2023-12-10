cc828e37e1f5cce21683096ccda4ac963ed71fdf
Inna Ladyzhenskaia <pinguinil2000@gmail.com>
2023-12-10 13:19:39 +0300

    создает автоматиацию

--- styles.js
+++ styles.js
@@ -6,8 +6,8 @@ import csso from 'postcss-csso';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 
-
 // Styles
+
 
 export const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
@@ -19,30 +19,4 @@ export const styles = () => {
     ]))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
-}
-
-// Server
-
-const server = (done) => {
-  browser.init({
-    server: {
-      baseDir: 'source'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
-}
-
-// Watcher
-
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-}
-
-
-export default gulp.series(
-  styles, server, watcher
-);
+};
