0aea3a24c00dca51c9190d4478ebf36cba71ea93
Inna Ladyzhenskaia <pinguinil2000@gmail.com>
2023-12-13 14:37:12 +0300

    делаает сборку

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,15 @@ import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import { stacksvg } from "gulp-stacksvg";
+import { deleteAsync } from 'del'
 
 
 // Styles
@@ -17,16 +24,79 @@ export const styles = () => {
       autoprefixer(),
       csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+//HTML
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+//Scripts
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'));
+}
+
+//Images
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'));
+}
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(gulp.dest('build/img'));
+}
+
+//WebP
+const createWebP = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh({ webp: {} }))
+    .pipe(gulp.dest('build/img'));
+}
+
+//Svg
+const svg = () => {
+  return gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'))
+}
+
+const makeStack = () => {
+  return gulp.src('source/img/icons/*.svg')
+    .pipe(svgo())
+    .pipe(stacksvg())
+    .pipe(gulp.dest('build/img/icons'))
+}
+
+//Copy
+const copy = (done) => {
+  gulp.src(['source/fonts/**/*.{woff2,woff}', 'source/*.ico', 'source/*.webmanifest',],
+    { base: 'source' }
+  )
+    .pipe(gulp.dest('build'))
+  done();
+}
+
+//Clean
+const clean = () => {
+  return deleteAsync('build');
+};
+
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -35,14 +105,51 @@ const server = (done) => {
   done();
 }
 
+//Reload
+
+const reload = (done) => {
+  browser.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/script.js', gulp.series(scripts));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
 
-export default gulp.series(
-  styles, server, watcher
+//Build
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    svg,
+    makeStack,
+    createWebP
+  ),
 );
+
+//Default
+export default gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    svg,
+    makeStack,
+    createWebP
+  ),
+  gulp.series(
+    server,
+    watcher,
+  ));
